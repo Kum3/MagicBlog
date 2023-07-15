a7cdd14ebe3d6e0b7fad37f4d91182b69a85c192
ayush <ayushkarwal@gmail.com>
2023-07-15 23:33:30 +0530

    Updated requirements.txt, launch.json, settings and added models as well.

--- MagicBlog/MagicBlog/settings.py
+++ MagicBlog/MagicBlog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'MagicBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MagicBlog',
+        'USER': 'postgres',
+        'PASSWORD': 'xp2#$kal1',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
--- MagicBlog/blogs/models.py
+++ MagicBlog/blogs/models.py
@@ -1,3 +1,51 @@
+""" """
+
+#normal imports
+import uuid
+#structured imports
 from django.db import models
 
 # Create your models here.
+CATEGORY_CHOICES = [
+]
+
+class User(models.Model):
+    """
+    User class.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    username = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.EmailField()
+
+
+class Blog(models.Model):
+    """
+    Blog class.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    summary = models.CharField(max_length=200)
+    description =  models.TextField()
+    image = models.ImageField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    """
+    Like class.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    """
+    Comment class.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    text = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
